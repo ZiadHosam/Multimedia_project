c4bf23adad6153420c5cc68a1e1e691cd40a19aa
wufflez <modyhamody2332@gmail.com>
2025-06-11 03:30:26 -0900

    edits

--- Source/Form1.cs
+++ Source/Form1.cs
@@ -28,40 +28,45 @@ namespace WindowsFormsApp24
         bool isDownPressed = false;
         bool isShiftPressed = false;
         bool isSpacePressed = false;
-
+        bool isFPressed = false;
         int ctTick = 0;
+        int FireRate = 0;
+        int BulletSpeed = 50;
+        int MoveLock = 0;
         public Form1()
         {
             this.WindowState = FormWindowState.Maximized;
             Load += Form1_Load;
             Paint += Form1_Paint;
-            KeyDown += move_keydown;
+            KeyDown += Form1_KeyDown;
             KeyUp += Form1_KeyUp;
-            KeyDown += Form1_KeyDown1;
-            tt.Interval = 85;
+            tt.Interval = 95;
             tt.Start();
             tt.Tick += Tt_Tick;
         }
-
 
         private void Form1_Load(object sender, EventArgs e)
         {
             CreateHero();
             off = new Bitmap(ClientSize.Width, ClientSize.Height);
         }
+
         private void Tt_Tick(object sender, EventArgs e)
         {
+            Move();//run,walk
             Bullets();
             Jump();
             Gravity();
-            Move();//run,walk
             Lazer();
-
+            AnimateHero();
+            CoolDowns();
+            
             ctTick++;
+
             DrawDubb(this.CreateGraphics());
         }
 
-        private void move_keydown(object sender, KeyEventArgs e)
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
             switch (e.KeyCode)
             {
@@ -77,14 +82,12 @@ namespace WindowsFormsApp24
                 case Keys.Up:
                     isUpPressed = true;
                     break;
-            }
-        }
-        private void Form1_KeyDown1(object sender, KeyEventArgs e)
-        {
-            switch (e.KeyCode)
-            {
                 case Keys.F:
-                    createbullets();
+                    isFPressed = true;
+                    if (FireRate == 0)
+                    {
+                        createbullets();
+                    }
                     break;
                 case Keys.G:
                     createlazer();
@@ -98,6 +101,7 @@ namespace WindowsFormsApp24
                     break;
             }
         }
+        
         private void Form1_KeyUp(object sender, KeyEventArgs e)
         {
             if (e.KeyCode == Keys.Left) isLeftPressed = false;
@@ -128,14 +132,24 @@ namespace WindowsFormsApp24
                 /*img = new Bitmap("Lwalk" + i + ".png");// 3shan a5od dimensions el pic
                 img.MakeTransparent(img.GetPixel(0, 0));
                 pnn.imgs.Add(img);*/
-
+                img = new Bitmap("jazz/Rwalk_" + i + ".png");
+                img.MakeTransparent(img.GetPixel(0, 0));
+                pnn.walkR.Add(img);
+                
                 img = new Bitmap("jazz/Lwalk_" + i + ".png");
                 img.MakeTransparent(img.GetPixel(0, 0));
                 pnn.walkL.Add(img);
 
-                img = new Bitmap("jazz/Rwalk_" + i + ".png");
-                img.MakeTransparent(img.GetPixel(0, 0));
-                pnn.walkR.Add(img);
+                if(i < 2)
+                {
+                    img = new Bitmap("jazz/Rshoot_" + i + ".png");
+                    img.MakeTransparent(img.GetPixel(0, 0));
+                    pnn.shootR.Add(img);
+
+                    img = new Bitmap("jazz/Lshoot_" + i + ".png");
+                    img.MakeTransparent(img.GetPixel(0, 0));
+                    pnn.shootL.Add(img);
+                }
             }
             /*pnn.animations.Add(standRight);
             pnn.animations.Add(standLeft);
@@ -149,79 +163,50 @@ namespace WindowsFormsApp24
             LHero.Add(pnn);
         }
 
-        void createbullets()
-        {//mfrod nzbot el x,y el bttl3 mnha el bullet 3la 7sb bases fen
-            cActor pnn = new cActor();
-            //pnn.x = LHero[0].X + LHero[0].imgs[LHero[0].iFrame].Width;
-            //pnn.y = LHero[0].Y + LHero[0].imgs[LHero[0].iFrame].Height / 2 + 8;
-            pnn.x = LHero[0].X;
-            pnn.y = LHero[0].Y;
-            pnn.w = 5;
-            pnn.h = 0;
-            pnn.Life = 1;
-            if (LHero[0].xDir == 1)
+        void Move()
+        {
+            /*if(MoveLock != 0)
             {
-                pnn.dir = 1;
+                return;
             }
             else
             {
-                pnn.dir = 0;
-            }
-            bullet.Add(pnn);
-        }
-        void Bullets()
-        {
-            for (int i = 0; i < bullet.Count; i++)
+                MoveLock = 1;
+            }*/
+
+
+            // l->r works but r->l doesnt
+            CMultiImageActor hero = LHero[0];
+            double run = 1;
+            if(isShiftPressed == true)
             {
-                if (bullet[i].Life == 1)
-                {
-                    if (bullet[i].dir == 1)
-                    {
-                        bullet[i].x += 20;
-                    }
-                    else
-                    {
-                        bullet[i].x -= 20;
-                    }
-                    if (bullet[i].x >= ClientSize.Width || bullet[i].x <= 0)//limit
-                    {
-                        bullet[i].Life = 0;//life kda mlhash lzma now bs mfrod lyha st5dam later
-                        bullet.RemoveAt(i);
-                    }
-                }
+                run = 1.5;
             }
+            if (isRightPressed == true)
+            {
+                if(hero.xDir == -1)
+                {
+                    hero.iFrame = -1;
+                }
+                hero.xDir = 1;
+                hero.X += (int)(hero.speed * run) * hero.xDir;
+                //LHero[0].iFrame = (LHero[0].iFrame + 1) % 4;
+                //LHero[0].currentAnimation = 2;
+            }
+            else if (isLeftPressed == true)
+            {
+                if (hero.xDir == 1)
+                {
+                    hero.iFrame = -1;
+                }
+                LHero[0].xDir = -1;
+                hero.X += (int)(hero.speed * run) * hero.xDir;
+                //LHero[0].iFrame = (LHero[0].iFrame + 1) % 4;
+                //LHero[0].currentAnimation = 3;
+            }
+            
         }
 
-        void createlazer()
-        {
-            int width = 200,margin=10;
-            cActor pnn = new cActor();
-            if (LHero[0].xDir == 1)
-            {
-                pnn.x = LHero[0].X + LHero[0].imgs[LHero[0].iFrame].Width;
-            }
-            else
-            {
-                pnn.x = LHero[0].X - width+ margin;
-            }
-            pnn.y = LHero[0].Y + LHero[0].imgs[LHero[0].iFrame].Height / 2 + 11;
-            pnn.w = width;
-            pnn.h = 3;
-            pnn.Life = 3;
-
-            lazer.Add(pnn);
-        }
-        void Lazer()
-        {
-            if (lazer.Count > 0)
-            {
-                lazer[lazer.Count - 1].Life--;
-                if (lazer[lazer.Count - 1].Life <= 0)
-                {
-                    lazer.RemoveAt(lazer.Count - 1);
-                }
-            }
-        }
         void Jump()
         {
             //mmkn n5lyh fluid aktr lw el jump 3la 7sb days ymen wla shmal
@@ -252,6 +237,135 @@ namespace WindowsFormsApp24
                 LHero[0].Y -= 50;
             }
         }
+
+        private void AnimateHero()
+        {
+            CMultiImageActor hero = LHero[0];
+            int curr = 0;
+
+            if (isShiftPressed == true)
+            {
+            }
+            if (isRightPressed == true)
+            {
+                //LHero[0].xDir = 1;
+                LHero[0].currentAnimation = 2;
+            }
+            else if (isLeftPressed == true)
+            {
+                //LHero[0].xDir = -1;
+                LHero[0].currentAnimation = 3;
+            }
+            else
+            {
+                hero.iFrame = 0;
+                if (hero.xDir == 1)
+                {
+                    LHero[0].currentAnimation = 0;
+                }
+                else
+                {
+                    hero.currentAnimation = 1;
+                }
+            }
+
+            /*if (isLeftPressed == false && isRightPressed == false)
+            {
+                hero.iFrame = 0;
+                if (hero.xDir == 1)
+                {
+                    hero.currentAnimation = 4;
+                }
+                else
+                {
+                    hero.currentAnimation = 5;
+                }
+            }*/
+
+            hero.iFrame++;
+        }
+
+        void createbullets()
+        {
+            //mfrod nzbot el x,y el bttl3 mnha el bullet 3la 7sb bases fen
+            FireRate = 5;
+
+            cActor pnn = new cActor();
+            //pnn.x = LHero[0].X + LHero[0].imgs[LHero[0].iFrame].Width;
+            //pnn.y = LHero[0].Y + LHero[0].imgs[LHero[0].iFrame].Height / 2 + 8;
+
+            pnn.x = LHero[0].X;
+            pnn.y = LHero[0].Y;
+            pnn.w = 5;
+            pnn.h = 0;
+            pnn.Life = 1;
+            if (LHero[0].xDir == 1)
+            {
+                pnn.dir = 1;
+            }
+            else
+            {
+                pnn.dir = 0;
+            }
+            bullet.Add(pnn);
+        }
+
+        void Bullets()
+        {
+            for (int i = 0; i < bullet.Count; i++)
+            {
+                if (bullet[i].Life == 1)
+                {
+                    if (bullet[i].dir == 1)
+                    {
+                        bullet[i].x += BulletSpeed;
+                    }
+                    else
+                    {
+                        bullet[i].x -= BulletSpeed;
+                    }
+                    //different than the game
+                    if (bullet[i].x >= ClientSize.Width || bullet[i].x <= 0)//limit
+                    {
+                        bullet[i].Life = 0;//life kda mlhash lzma now bs mfrod lyha st5dam later
+                        bullet.RemoveAt(i);
+                    }
+                }
+            }
+        }
+
+        void createlazer()
+        {
+            int width = 200,margin=10;
+            cActor pnn = new cActor();
+            if (LHero[0].xDir == 1)
+            {
+                pnn.x = LHero[0].X + LHero[0].imgs[LHero[0].iFrame].Width;
+            }
+            else
+            {
+                pnn.x = LHero[0].X - width+ margin;
+            }
+            pnn.y = LHero[0].Y + LHero[0].imgs[LHero[0].iFrame].Height / 2 + 11;
+            pnn.w = width;
+            pnn.h = 3;
+            pnn.Life = 3;
+
+            lazer.Add(pnn);
+        }
+
+        void Lazer()
+        {
+            if (lazer.Count > 0)
+            {
+                lazer[lazer.Count - 1].Life--;
+                if (lazer[lazer.Count - 1].Life <= 0)
+                {
+                    lazer.RemoveAt(lazer.Count - 1);
+                }
+            }
+        }
+
         void Gravity()
         {
             //clientsize.height/2 for testing (htt8yr)
@@ -276,51 +390,33 @@ namespace WindowsFormsApp24
                 flying = false;
             }
         }
-        void Move()
+
+        private void CoolDowns()
         {
-            CMultiImageActor hero = LHero[0];
-            double run = 1;
-            if(isShiftPressed == true)
+            if (FireRate > 0)
             {
-                run = 1.5;
+                FireRate--;
             }
-            if (isRightPressed == true)
+            if (MoveLock > 0)
             {
-                LHero[0].X += (int)(hero.speed * run);
-                if(hero.xDir == -1)
-                {
-                    hero.iFrame = -1;
-                }
-                LHero[0].xDir = 1;
-                //LHero[0].iFrame = (LHero[0].iFrame + 1) % 4;
-                hero.iFrame++;
-                LHero[0].currentAnimation = 2;
-            }
-            else if (isLeftPressed == true)
-            {
-                LHero[0].X -= (int)(hero.speed * run);
-                if (hero.xDir == 1)
-                {
-                    hero.iFrame = -1;
-                }
-                LHero[0].xDir = -1;
-                //LHero[0].iFrame = (LHero[0].iFrame + 1) % 4;
-                hero.iFrame++;
-                LHero[0].currentAnimation = 3;
-            }
-            else
-            {
-                hero.iFrame = 0;
-                if(hero.xDir == 1)
-                {
-                    LHero[0].currentAnimation = 0;
-                }
-                else
-                {
-                    hero.currentAnimation = 1;
-                }
+                MoveLock--;
             }
         }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            DrawDubb(e.Graphics);
+        }
+
+
+        void DrawDubb(Graphics g)
+        {
+            Graphics g2 = Graphics.FromImage(off);
+            DrawScene(g2);
+            g.DrawImage(off, 0, 0);
+
+        }
+
         void DrawScene(Graphics g2)
         {
             g2.Clear(Color.Black);
@@ -349,23 +445,9 @@ namespace WindowsFormsApp24
                 g2.DrawImage(frame, hero.X - (frame.Width / 2), hero.Y - (frame.Height / 2));
             }
 
-            
-
             //test line
             g2.DrawLine(Pens.LightGray, 0, ClientSize.Height / 2, ClientSize.Width, ClientSize.Height / 2);
         }
-        void DrawDubb(Graphics g)
-        {
-            Graphics g2 = Graphics.FromImage(off);
-            DrawScene(g2);
-            g.DrawImage(off, 0, 0);
-
-        }
-        private void Form1_Paint(object sender, PaintEventArgs e)
-        {
-            DrawDubb(e.Graphics);
-        }
-
     }
     public class cActor
     {
@@ -375,10 +457,22 @@ namespace WindowsFormsApp24
     {
         public int X, Y;
         public List<List<Bitmap>> animations = new List<List<Bitmap>>();
+        //0
         public List<Bitmap> standR = new List<Bitmap>();
+        //1
         public List<Bitmap> standL = new List<Bitmap>();
+        //2
         public List<Bitmap> walkR = new List<Bitmap>();
+        //3
         public List<Bitmap> walkL = new List<Bitmap>();
+        //4
+        public List<Bitmap> shootR = new List<Bitmap>();
+        //5
+        public List<Bitmap> shootL = new List<Bitmap>();
+        //6
+        public List<Bitmap> stopshootR = new List<Bitmap>();
+        //7
+        public List<Bitmap> stopshootL = new List<Bitmap>();
         public List<Bitmap> imgs;
         public int iFrame;
         public int xDir = 0; 
@@ -401,6 +495,8 @@ namespace WindowsFormsApp24
                 case 3:
                     ltmp = walkL;
                     break;
+                case 4:
+                    ltmp = shootR;
             }
             return ltmp;
         }
